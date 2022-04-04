e40cbae06caef81b1f4474026f2587e217dac540
FaizaNA07 <faizaabida44614@gmail.com>
2022-04-04 08:16:55 -0700

    praktikum6

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:sqlite_flutter/page/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tambahkan Item => Faiza Nur A. | 2031710068',
+      theme: ThemeData(
+        primarySwatch: Colors.orange
+      ),
+      home: Home(),
+    );
+  }
+}
